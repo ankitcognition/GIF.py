309a1b17b693b642cd0bdcf98bc7cc8015e6023c
Ankit Singh <singhji743900@gmail.com>
2026-06-21 18:17:00 +0530

    just learned this method

---
+++ create_gif.py
@@ -0,0 +1,24 @@
+import imageio.v3 as iio
+from PIL import Image
+import numpy as np
+
+filenames = ['image1.jpeg', 'image2.jpeg']
+
+images = []
+
+first = Image.open(filenames[0])
+size = first.size
+
+for filename in filenames:
+    img = Image.open(filename)
+    img = img.resize(size)     
+    images.append(np.array(img))
+
+iio.imwrite(
+    'team.gif',
+    images,
+    duration=0.5,
+    loop=0
+)
+
+print("GIF created successfully!")
